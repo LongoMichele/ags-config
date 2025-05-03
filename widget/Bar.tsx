ff7029a8473639eacde3635cee7382bd3591a2d9
LongoMichele <longomichele2015@gmail.com>
2025-05-03 13:05:19 +0200

    Code formatting

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -4,28 +4,26 @@ import { Variable } from "astal"
 const time = Variable("").poll(1000, "date")
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
-    return <window
-        className="Bar"
-        gdkmonitor={gdkmonitor}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={TOP | LEFT | RIGHT}
-        application={App}>
-        <centerbox>
-            <button
-                onClicked="echo hello"
-                halign={Gtk.Align.CENTER}
-            >
-                Welcome to AGS!
-            </button>
-            <box />
-            <button
-                onClicked={() => print("hello")}
-                halign={Gtk.Align.CENTER}
-            >
-                <label label={time()} />
-            </button>
-        </centerbox>
-    </window>
+  return <window
+    className="Bar"
+    gdkmonitor={gdkmonitor}
+    exclusivity={Astal.Exclusivity.EXCLUSIVE}
+    anchor={TOP | LEFT | RIGHT}
+    application={App}
+    child={<centerbox>
+      <button
+        onClicked="echo hello"
+        halign={Gtk.Align.CENTER}
+        label='Welcome to AGS!'
+      />
+      <box />
+      <button
+        onClicked={() => print("hello")}
+        halign={Gtk.Align.CENTER}
+        label={time()}
+      />
+    </centerbox>}
+  />
 }
